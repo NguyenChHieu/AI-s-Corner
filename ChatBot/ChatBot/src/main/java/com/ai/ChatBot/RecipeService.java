35d8cc97e0d4d1b934a9e5884e56f44c81ff9387
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-03 15:07:37 +0700

    recipe

--- ChatBot/ChatBot/src/main/java/com/ai/ChatBot/RecipeService.java
+++ ChatBot/ChatBot/src/main/java/com/ai/ChatBot/RecipeService.java
@@ -24,6 +24,8 @@ public class RecipeService {
                 Please consider the following dietary restrictions: {dietaryRestrictions}.
                 Please provide me with a detailed recipe that followed this structure:
                 including 1.Title, 2.List of ingredients, 3.Cooking instructions.
+                Don't format in markdown because i will display it in plain text, be basic
+                but neat. Thank you!
                 """;
         PromptTemplate promptTemplate = new PromptTemplate(template);
         Map<String, Object> params = Map.of(
--- ChatBot/chatbot-front-end/src/components/RecipeGen.js
+++ ChatBot/chatbot-front-end/src/components/RecipeGen.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 export default function RecipeGen() {
+    const [ingredients, setIngredients] = useState('');
+    const [cuisine, setCuisine] = useState('any')
+    const [dietaryRestrictions, setDietaryRestrictions] = useState('')
+    const [recipe, setRecipe] = useState('')
+            
+    const createRecipe = async () =>{
+        try{
+            const response = await fetch(`http://localhost:8080/recipe-creator?ingredients=${ingredients}&cuisine=${cuisine}&dietaryRestrictions=${dietaryRestrictions}`)
+            const data = await response.text()
+            console.log(data)
+            setRecipe(data)
+        }
+        catch (error){
+            console.log("Error generating recipe: ", error)
+        }
+    }
+
   return (
-    <h2>RecipeGen</h2>
+    <div>
+        <h2>RecipeGen</h2>
+
+        <input type="text"
+        value={ingredients}
+        onChange={e => setIngredients(e.target.value)}
+        placeholder="what's ur cooking?"/>
+
+        <input type="text"
+        value={cuisine}
+        onChange={e => setCuisine(e.target.value)}
+        placeholder="what's ur type?"/>
+
+        <input type="text"
+        value={dietaryRestrictions}
+        onChange={e => setDietaryRestrictions(e.target.value)}
+        placeholder="what should i avoid?"/>
+
+        <button onClick={createRecipe}>
+            Cookin!
+        </button>
+
+        <div className="output">
+            <pre className="recipe-text">{recipe}</pre>
+        </div>
+    </div>
+  
   )
 }
